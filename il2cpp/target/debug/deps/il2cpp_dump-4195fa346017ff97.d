46ed2031a0f477dd596c9a8906d6e7b06e78574f
ejacza <ejacza@users.noreply.github.com>
2026-07-28 20:50:27 +0000

    gitignore: exclude il2cpp/target/

--- il2cpp/target/debug/deps/il2cpp_dump-4195fa346017ff97.d
+++
@@ -1,8 +0,0 @@
-/opt/data/il2cpp/il2cpp/target/debug/deps/libil2cpp_dump-4195fa346017ff97.rmeta: src/lib.rs src/api.rs src/dump.rs src/tabledefs.rs
-
-/opt/data/il2cpp/il2cpp/target/debug/deps/il2cpp_dump-4195fa346017ff97.d: src/lib.rs src/api.rs src/dump.rs src/tabledefs.rs
-
-src/lib.rs:
-src/api.rs:
-src/dump.rs:
-src/tabledefs.rs:
